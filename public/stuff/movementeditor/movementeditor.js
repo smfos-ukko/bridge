06676df84e33c028c42eae103ff56303928e2dd9
Antti Valkama <73844297+smfos-ukko@users.noreply.github.com>
2026-05-11 02:19:23 +0300

    devings

--- public/stuff/movementeditor/movementeditor.js
+++ public/stuff/movementeditor/movementeditor.js
@@ -3,7 +3,44 @@ import { showMessage } from "../bridge.js";
 const movementData = {};
 const pageHtml = {};
 const pageSettings = {
-    selectedPage: 1
+    loaded: false,
+    selectedPage: 1,
+    sharedBoards: false
+};
+let movementTools;
+
+const changePage = (dir) => {
+    let pageChanged = false;
+    if (dir == 'prev') {
+        if (pageSettings.selectedPage > 1) pageSettings.selectedPage--;
+        pageChanged = true;
+        console.log(dir, pageSettings);
+    }    
+    if (dir == 'next') {
+        if (pageSettings.selectedPage < movementData.numberOfTables) pageSettings.selectedPage++;
+        pageChanged = true;
+                console.log(dir, pageSettings);
+
+    }  
+    if (pageChanged) {
+        document.getElementById('mvPaper').innerHTML = pageHtml[pageSettings.selectedPage];
+        document.getElementById('selectedPageIndicator').innerHTML = '<span>' + pageSettings.selectedPage + '</span>';
+    }
+};
+
+const updateTools = () => {
+    movementTools = document.getElementById('mvTools');
+    let toolHtml = `
+        <h3>Sivu</h3>
+        <div id="movPageSelector">
+            <button id="movPrevButton">⯇</button> 
+            <div id="selectedPageIndicator"><span>1</span></div>
+            <button id="movNextButton">⯈</button> 
+        </div>
+    `;
+    movementTools.innerHTML = toolHtml;
+    document.getElementById('movPrevButton').addEventListener('click', () => {changePage('prev')});
+    document.getElementById('movNextButton').addEventListener('click', () => {changePage('next')});
 };
 
 const decodeDeal = (letter) => {
@@ -22,10 +59,15 @@ const decodeDeal = (letter) => {
 
 const findSeat = (player, round) => {
     for (let tbl = 1; tbl <= movementData.numberOfTables; tbl++) {
-        console.log('table: ', tbl, ' player: ', player, ' round: ', round);
         if (movementData.tables[tbl][round][0] == player) return { table: tbl, direction: 'NS' };
         if (movementData.tables[tbl][round][1] == player) return { table: tbl, direction: 'EW' };
     }
+};
+
+const defineDeals = (dealSet) => {
+    const lastDeal = movementData.dealsPerRound * dealSet;
+    const firstDeal = lastDeal - movementData.dealsPerRound + 1;
+    return `${firstDeal}-${lastDeal}`;
 };
 
 const updateMovement = () => {
@@ -45,44 +87,88 @@ const updateMovement = () => {
     }
     
     //define default movement
-    const transitionCounts = {};
     for (let tbl = 1; tbl <= movementData.numberOfTables; tbl++) {
-        transitionCounts[tbl] = {
+        const transitionCounts = {
             NS: {},
             EW: {}
         };
         for (let rnd = 1; rnd < movementData.numberOfRounds; rnd++) {
             const transfer = movementData.transfers[tbl][rnd];
-            const NSkey = `${transfer[0]}-${transfer[1]}`;
-            const EWkey = `${transfer[2]}-${transfer[3]}`;
-            transitionCounts[tbl].NS[NSkey] = (transitionCounts[tbl].NS[NSkey] || 0) + 1;
-            transitionCounts[tbl].EW[EWkey] = (transitionCounts[tbl].EW[EWkey] || 0) + 1;
+            const NSkey = `→ ${transfer[0]} ${transfer[1]}`;
+            const EWkey = `→ ${transfer[2]} ${transfer[3]}`;
+            transitionCounts.NS[NSkey] = (transitionCounts.NS[NSkey] || 0) + 1;
+            transitionCounts.EW[EWkey] = (transitionCounts.EW[EWkey] || 0) + 1;
         }
-    }
-    let maxKey = null;
-    let maxCount = 0;
-    for (const key in transitionCounts) {
-        if (transitionCounts[key] > maxCount) {
-            maxCount = transitionCounts[key];
-            maxKey = key;
+
+        let maxKey = null;
+        let maxCount = 0;
+        for (const key in transitionCounts.NS) {
+            if (transitionCounts.NS[key] > maxCount) {
+                maxCount = transitionCounts.NS[key];
+                maxKey = key;
+            }
         }
+        movementData.transfers[tbl].NSdefault = maxKey;
+        maxKey = null;
+        maxCount = 0;
+        for (const key in transitionCounts.EW) {
+            if (transitionCounts.EW[key] > maxCount) {
+                maxCount = transitionCounts.EW[key];
+                maxKey = key;
+            }
+        }
+        movementData.transfers[tbl].EWdefault = maxKey;
+        console.log(transitionCounts, maxKey, maxCount, movementData);
     }
-    console.log(transitionCounts, maxKey, maxCount);
 
     renderMovement();
+    updateTools();
+};
+
+const scaleTable = () => {
+    const mtMain = document.getElementById('mtMain');
+    console.log('client ', mtMain.clientHeight);
 };
 
 const renderMovement = () => {
     const mvPaper = document.getElementById('mvPaper');
+    let isOdd = true;
 
     for (let p = 1; p <= movementData.numberOfTables; p++) {
+        //draw table
+        let movementTable = '<div class="mtMain">';
+        movementTable += `
+            <div class="mtCell mth">Kierros</div>
+            <div class="mtCell mth">N/S</div>
+            <div class="mtCell mth">E/W</div>
+            <div class="mtCell mth">Jaot</div>
+        `;
+        for (let r = 1; r <= movementData.numberOfRounds; r++) {
+            movementTable += `<div class="mtCell ${isOdd ? 'mtOdd' : 'mtEven'} movTableRound">` + r + '</div>';
+            movementTable += `<div class="mtCell ${isOdd ? 'mtOdd' : 'mtEven'} movTableNS">` + movementData.tables[p][r][0] + '</div>';
+            movementTable += `<div class="mtCell ${isOdd ? 'mtOdd' : 'mtEven'} movTableEW">` + movementData.tables[p][r][1] + '</div>';
+            movementTable += `<div class="mtCell ${isOdd ? 'mtOdd' : 'mtEven'} movTableDeals">` + defineDeals(movementData.tables[p][r][2]) + '</div>';
+            isOdd ? isOdd = false : isOdd = true;
+        }
+        movementTable += '</div>';
+
+        //draw paper
         let mov = `
-            <div class="movTop">
-                <div class="movTableNumber movNorth"><h1>${p}</h1></div>
-                <div class="movNS movTransfer movNorth"><h2>N</h2><span></span></div>
+            <div class="movTop movSegment">
+                <div class="movTableNumber movNumWest"><h1>${p}</h1></div>
+                <div class="movNS movDefaultTransfer movNorth"><h2>N</h2><span>${movementData.transfers[p].NSdefault}</span></div>
+                <div class="movTableNumber movNumNorth"><h1>${p}</h1></div>
             </div>
-            <div class="movMid"></div>
-            <div class="movBot"></div>
+            <div class="movMid movSegment">
+                <div class="movEW movDefaultTransfer movWest"><h2>W</h2><span>${movementData.transfers[p].EWdefault}</span></div>
+                <div class="movTransferTable">${movementTable}</div>
+                <div class="movEW movDefaultTransfer movEast"><h2>E</h2><span>${movementData.transfers[p].EWdefault}</span></div>
+            </div>
+            <div class="movBot movSegment">
+                <div class="movTableNumber movNumSouth"><h1>${p}</h1></div>
+                <div class="movNS movDefaultTransfer movSouth"><h2>S</h2><span>${movementData.transfers[p].NSdefault}</span></div>
+                <div class="movTableNumber movNumEast"><h1>${p}</h1></div>
+            </div>
         `;
         pageHtml[p] = mov;
     }
@@ -146,6 +232,7 @@ export function movementEditor() {
                     }
 
                     console.log(file, movementData, lines);
+                    pageSettings.loaded = true;
                     updateMovement();
                 }
 
@@ -161,6 +248,10 @@ export function movementEditor() {
 
     document.getElementById('mvOpenFileButton').addEventListener('click', () => {
         openMovementFile();
+    });
+
+    document.addEventListener('DOMContentLoaded', () => {
+        scaleTable();
     });
 
 }
